d8f961b8e82ebc101d6803de43dc470df95ae98a
Iván Calle <ivancalle@sipay.es>
2018-03-01 09:14:34 +0100

    [HSDK-4] unregister functionality

---
+++ sipay/responses/Unregister.php
@@ -0,0 +1,8 @@
+<?php
+namespace Sipay\Responses;
+
+class Unregister extends Response{
+    public function __construct($request, $response) {
+        $this->set_common($request, $response);
+    }
+}
