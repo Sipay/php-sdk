58d3e34f868082c2c79b6e080eb1c3b714397e44
Darcy <darcyzerpa@sipay.es>
2018-03-07 18:48:24 +0100

    removing sensitive data

--- examples/refund.php
+++ examples/refund.php
@@ -3,9 +3,10 @@ require_once realpath(__DIR__.'/../src/autoload.php');
 
 $ecommerce = new \Sipay\Ecommerce(realpath(__DIR__."/../etc/config.ini"));
 
+
 $amount = new \Sipay\Amount(200, 'EUR');
 
-$card = new \Sipay\Paymethods\Card('6712009000000205', 2018, 12);
+$card = new \Sipay\Paymethods\Card('4242424242424242', 2018, 12);
 
 $options = array(
   'order' => 'order-test',
@@ -13,6 +14,7 @@ $options = array(
   'token' => 'new-token'
 );
 
+// Devolución
 $refund = $ecommerce->refund($card, $amount, $options);
 
 if($refund->code == 0) {
@@ -23,6 +25,8 @@ if($refund->code == 0) {
 
 $stored_card = new \Sipay\Paymethods\StoredCard('new-token');
 
+
+// Devolución
 $refund2 = $ecommerce->refund($stored_card, $amount, $options);
 
 if($refund2->code == 0) {
@@ -31,8 +35,9 @@ if($refund2->code == 0) {
     print("Error: ".$refund2->description."\n");
 }
 
-$fast_pay = new \Sipay\Paymethods\FastPay('0f266784e7ba4e438040fdd1dbbfcd73');
+$fast_pay = new \Sipay\Paymethods\FastPay('50d79105f6f848f9ad30f9c5970adf7c');
 
+// Devolución
 $refund3 = $ecommerce->refund($fast_pay, $amount, $options);
 
 if($refund3->code == 0) {
@@ -42,7 +47,7 @@ if($refund3->code == 0) {
 }
 
 
-
+// Autorización
 $auth = $ecommerce->authorization($card, $amount, $options);
 
 if($auth->code == 0) {
@@ -52,6 +57,7 @@ if($auth->code == 0) {
 }
 
 
+// Devolución
 $refund4 = $ecommerce->refund($auth->transaction_id, $amount, $options);
 
 if($refund4->code == 0) {
